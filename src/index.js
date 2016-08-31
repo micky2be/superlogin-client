583b8dbd4d820a2fd6bfaad43b97a10f0ce2319a
Peter Uithoven <peter@peteruithoven.nl>
2016-08-31 10:42:22 +0200

    Creating fresh axios instance on configure
    Another, more general way to clear the interceptors from previous configure calls

--- src/index.js
+++ src/index.js
@@ -28,7 +28,6 @@ class Superlogin extends EventEmitter {
 		this._config = {};
 		this._refreshCB = this.refresh;
 		this.refreshInProgress = false;
-		this._http = axios.create();
 	}
 
 	configure(config = {}) {
@@ -68,6 +67,10 @@ class Superlogin extends EventEmitter {
 	}
 
 	_httpInterceptor() {
+		// Create fresh axios instance, forgetting
+		// interceptors from a previous configure call
+		this._http = _axios2.default.create();
+		
 		const request = req => {
 			const config = this.getConfig();
 			const session = this.getSession();
@@ -98,10 +101,6 @@ class Superlogin extends EventEmitter {
 			}
 			return Promise.reject(response);
 		};
-
-		// clear interceptors from a previous configure call
-		this._http.interceptors.request.handlers = [];
-		this._http.interceptors.response.handlers = [];
 		
 		this._http.interceptors.request.use(request.bind(this));
 		this._http.interceptors.response.use(null, responseError.bind(this));
