c9d5ebd57d234407f66fa5fd7776a5b35b0e4b82
micky2be <micky2be@gmail.com>
2016-09-28 15:48:24 +0900

    * Added typescript definition

--- superlogin-client.d.ts
+++ superlogin-client.d.ts
@@ -1,33 +1,47 @@
+import { EventEmitter2 } from 'eventemitter2';
+import { AxiosInstance } from 'axios';
+
 export interface Promise<V> {
 	then<R1, R2>(onFulfilled: (value: V) => R1 | Promise<R1>, onRejected: (error: any) => R2 | Promise<R2>): Promise<R1 | R2>;
 	then<R>(onFulfilled: (value: V) => R | Promise<R>): Promise<R>;
 	catch<R>(onRejected: (error: any) => R | Promise<R>): Promise<R>;
 }
 
-interface Http {
-	post(path: string, data: any): Promise<any>;
-	get(path: string): Promise<any>;
-}
-
-type loginType = { username: string, password: string };
-type registerType = { username: string, password: string, confirmPassword: string };
-type resetType = { password: string, confirmPassword: string, token: string };
-
-interface SuperLoginClient extends EventEmitter {
-	getHttp(): Http;
-	validateSession();
-	configure(options: any);
-	login(loginType);
-	logout();
-	socialAuth(social: string);
-	register(registerType);
-	setSession(session: any);
-	forgotPassword(email: string);
-	resetPassword(resetType);
+interface SuperLoginClient extends EventEmitter2.emitter {
+	configure: (options: any) => void;
+	authenticated: () => boolean;
+	authenticate: () => Promise<any>;
+	getConfig: () => any;
+	validateSession: () => void;
+	getSession: () => any;
+	setSession: (session: any) => void;
+	deleteSession: () => void;
+	getDbUrl: (dbName: string) => string;
+	confirmRole: (role: string) => boolean;
+	confirmAnyRole: (possibleRoles: Array<string>) => boolean;
+	confirmAllRoles: (requiredRoles: Array<string>) => boolean;
+	refresh: () => void;
+	checkRefresh: () => void;
+	checkExpired: () => void;
+	login: (login: { username: string, password: string }) => Promise<any>;
+	register: (register: { username: string, name?: string, email?: string, password: string, confirmPassword: string } | any) => Promise<any>;
+	logout: (message?: string) => Promise<any>;
+	logoutAll: (message?: string) => Promise<any>;
+	logoutOthers: () => Promise<any>;
+	socialAuth: (provider: string) => Promise<any>;
+	tokenSocialAuth: (provider: string, accessToken: string) => Promise<any>;
+	link: (provider: string) => Promise<any>;
+	tokenLink: (provider: string, accessToken: string) => Promise<any>;
+	unlink: (provider: string) => Promise<any>;
+	verifyEmail: (token: string) => Promise<any>;
+	forgotPassword: (email: string) => Promise<any>;
+	resetPassword: (reset: { password: string, confirmPassword: string, token: string }) => Promise<any>;
+	changePassword: (change: { currentPassword?: string, newPassword: string, confirmPassword: string, token: string }) => Promise<any>;
+	changeEmail: (email: string) => Promise<any>;
+	validateUsername: (username: string) => Promise<any>;
+	validateEmail: (email: string) => Promise<any>;
+	getHttp: () => AxiosInstance;
 }
 
 declare const client: SuperLoginClient;
-
-declare module 'superlogin-client' {	
-	export default client;
-}
+export default client;
